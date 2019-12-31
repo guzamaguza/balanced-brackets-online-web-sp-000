3888d819f2a8a682979839df9713a7990dbab54f
Mark A Guza <guzamaguza@gmail.com>
2019-12-31 16:51:00 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,44 @@
+function isBalanced(expr){
+  var S = [];
+  for(let i=0;i<expr.length;i++){
+    var currentChar = expr[i];
+    if(currentChar === '{' || currentChar === '(' || currentChar === '[' ){
+      S.push(currentChar);
+    }
+    if(currentChar === '}' || currentChar === ')' || currentChar === ']' ){
+      if(currentChar === '}'){
+        if(S.includes('{')){
+          continue 
+        }else{
+          return false 
+          break 
+        }
+      }else if(currentChar === ')'){
+        if(S.includes('(')){
+          continue 
+        }else{
+          return false 
+          break 
+        }
+      }else if(currentChar === ']'){
+        if(S.includes('[')){
+          continue 
+        }else{
+          return false 
+          break 
+        }
+      }
+    }
+  }
+  
+}
+
+
+
+
+// Algorithm:
+// 1) Declare a character stack S.
+// 2) Now traverse the expression string exp.
+//     a) If the current character is a starting bracket (‘(‘ or ‘{‘ or ‘[‘) then push it to stack.
+//     b) If the current character is a closing bracket (‘)’ or ‘}’ or ‘]’) then pop from stack and if the popped character is the matching starting bracket then fine else parenthesis are not balanced.
+// 3) After complete traversal, if there is some starting bracket left in stack then “not balanced”
